81278284c995c1a478fff10eed3efca3651cba4b
Kt.codes <tarajkristian@gmail.com>
2021-06-15 13:04:26 +0200

    fixed bugs

--- wp-content/themes/academia/functions.php
+++ wp-content/themes/academia/functions.php
@@ -33,4 +33,4 @@ foreach ( $understrap_includes as $file ) {
 	}
 	require_once $filepath;
 }
-wp_enqueue_script( 'script', get_template_directory_uri() . '/js/custom.js', 1.1, true);
+wp_enqueue_script( 'script', get_template_directory_uri() . '/js/custom2.js', 1.1, true);
